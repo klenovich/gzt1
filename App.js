fe6c8966dbe01ea21f45cb4a70d757e09597fd50
klucas12345 <klucas12345@gmail.com>
2023-09-02 12:58:53 -0400

    new file:   .ChatScreen.js.swp  	modified:   App.js  	new file:   yarn.lock

--- App.js
+++ App.js
@@ -1,39 +1,8 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer, { register } from "./UserSlice";
-import RegistrationScreen from "./RegistrationScreen";
-import IRCChatScreen from "./IRCChatScreen";
-
-const Stack = createStackNavigator();
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
+import React from 'react';
+import ChatScreen from './ChatScreen';
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Register">
-          <Stack.Screen
-            name="Register"
-            component={RegistrationScreen}
-            options={{ title: 'Registration' }}
-          />
-          <Stack.Screen
-            name="ChatScreen"
-            component={IRCChatScreen}
-            options={{ title: 'Chat' }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+  return <ChatScreen />
 };
 
 export default App;
